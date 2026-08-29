35ae2fd2d18b3b8e2cd9ded443f8d251a68ec05f
Thauan Miranda <thauan10saopaulo@gmail.com>
2026-08-28 23:54:58 -0300

    adicionando as variáveis de ambiente no projeto

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    ambiente: 'dev',
+    url: 'http://localhost:8081/rest'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    ambiente: 'prd',
+    url: 'http://localhost:8081/rest'
+};
